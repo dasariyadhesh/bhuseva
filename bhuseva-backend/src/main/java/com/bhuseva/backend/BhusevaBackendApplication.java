423751c31c4de910193f7e7ec2f06183acb65850
Yadhesh DG <dasariyadhesh@gmail.com>
2025-06-03 20:34:10 +0530

    Initial application commit

---
+++ bhuseva-backend/src/main/java/com/bhuseva/backend/BhusevaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bhuseva.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhusevaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhusevaBackendApplication.class, args);
+	}
+
+}
